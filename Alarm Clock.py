df1e5956c53826391bd863b795bd721a6c41b527
AJFlareau <andrew.flareau@myds.us>
2017-01-13 10:46:30 -0800

    first commit

---
+++ Alarm Clock.py
@@ -0,0 +1,33 @@
+from Tkinter import * #gives us access to everything in the Tkinter class
+import tkMessageBox
+from PIL import Image, ImageTk
+from datetime import datetime
+from threading import *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root = Tk()
+root.title ("Multiple Alarm Clock With Choice Of Specialized Music Made By The Fresh Oscaracso")
+
+
+
+
+
+
+
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+
+mainloop() #causes the windows to display on the screen until program closes
