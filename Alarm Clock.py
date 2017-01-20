3f16cde68fcf3f640fe2692d4b55ac8bd1d06dfc
AJFlareau <andrew.flareau@myds.us>
2017-01-20 10:32:14 -0800

    update 1/20/2017

--- Alarm Clock.py
+++ Alarm Clock.py
@@ -12,22 +12,39 @@ from threading import *
 
 
 
+def countdown(count): 
+    label['text'] = count
 
+    if count > 0:
+        top.after(1000, countdown,count-1)
 
+top = tkinter.Tk()
+top.geometry("700x100")
+hoursT=tkinter.Label(top, text="Hours:")
+hoursE=tkinter.Entry(top)
+minuteT=tkinter.Label(top, text="Minutes:")
+minuteE=tkinter.Entry(top)
+secondT=tkinter.Label(top, text="Seconds:")
+secondE=tkinter.Entry(top)
+hoursT.grid(row=1,column=1)
+hoursE.grid(row=1,column=2)
+minuteT.grid(row=1,column=3)
+minuteE.grid(row=1,column=4)
+secondT.grid(row=1,column=5)
+secondE.grid(row=1,column=6)
+label = tkinter.Label(top)
+label.grid(row=3)
 
+t=(int(hoursE.get())*360+int(minuteT.get())*60+int(secondE.get())
+button=tkinter.Button(top,text="Start Timer",command=lambda:None)
+button.grid(row=2)
 
+def updateButton():
+    hour,min,sec=hoursE.get(),minuteT.get(),secondE.get()
+    if hour.isdigit() and min.isdigit() and sec.isdigit():
+        time=int(hour)*360+int(min)*60+int(sec)
+        button.configure(command=lambda count=time:countdown(count))
 
-root = Tk()
-root.title ("Multiple Alarm Clock With Choice Of Specialized Music Made By The Fresh Oscaracso")
-
-
-
-
-
-
-
-
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-
+for widget in (hoursE,minuteT,secondE):
+    widget.bind("<FocusOut>", updateButton)
 mainloop() #causes the windows to display on the screen until program closes
